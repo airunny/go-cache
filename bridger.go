68328a1a3687ce4a77affc0c61a8e015e466601a
李彦兵 <liyanbing@shuinfo.com>
2022-05-31 19:42:24 +0800

    feat: add bridger

---
+++ bridger.go
@@ -0,0 +1,149 @@
+package go_cache
+
+import (
+	"context"
+	"log"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/golang/protobuf/proto"
+	"github.com/liyanbing/go-cache/cacher/lru"
+	"github.com/liyanbing/go-cache/cacher/memory"
+
+	redisCache "github.com/liyanbing/go-cache/cacher/redis"
+)
+
+type Bridge interface {
+	Cache
+	FetchWithJson(ctx context.Context, key string, fetcher Fetcher, model interface{}) (interface{}, error)
+	FetchWithString(ctx context.Context, key string, fetcher Fetcher) (string, error)
+	FetchWithProtobuf(ctx context.Context, key string, fetcher Fetcher, model interface{}) (proto.Message, error)
+	FetchWithNumber(ctx context.Context, key string, fetcher Fetcher) (float64, error)
+	FetchWithArray(ctx context.Context, key string, fetcher Fetcher, model interface{}) (interface{}, error)
+	FetchWithIncludeKeys(ctx context.Context, output CacheValueOutput, empty EmptyCache, dec Decoder, otherKeys ...string) error
+	FetchWithKeys(ctx context.Context, keys ...string) ([]interface{}, error)
+}
+
+type cacheType int8
+
+const (
+	cacheTypeRedis cacheType = iota
+	cacheTypeMemory
+	cacheTypeLRU
+	cacheTypeCustom
+)
+
+type option struct {
+	cacheType        cacheType
+	redisCli         *redis.Client
+	cache            Cache
+	memoryMaxEntries int32
+	lruMaxEntries    int
+}
+
+func defaultOption() option {
+	return option{
+		cacheType:     cacheTypeLRU,
+		lruMaxEntries: 100,
+	}
+}
+
+type BridgeOption func(option)
+
+func WithRedis(cli *redis.Client) BridgeOption {
+	return func(o option) {
+		o.cacheType = cacheTypeRedis
+		o.redisCli = cli
+	}
+}
+
+func WithMemory(maxEntries int32) BridgeOption {
+	return func(o option) {
+		o.cacheType = cacheTypeMemory
+		o.memoryMaxEntries = maxEntries
+	}
+}
+
+func WithLRU(maxEntries int32) BridgeOption {
+	return func(o option) {
+		o.cacheType = cacheTypeLRU
+
+	}
+}
+
+func WithCache(cache Cache) BridgeOption {
+	return func(o option) {
+		o.cacheType = cacheTypeCustom
+		o.cache = cache
+	}
+}
+
+var (
+	_ Bridge = (*bridger)(nil)
+)
+
+type bridger struct {
+	cacheType cacheType
+	Cache
+	redisCli redis.Client
+}
+
+func NewBridge(opts ...BridgeOption) Bridge {
+	o := defaultOption()
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	switch o.cacheType {
+	case cacheTypeRedis:
+		if o.redisCli == nil {
+			log.Fatal("empty redis client")
+		}
+		o.cache = redisCache.NewRedisCache(o.redisCli)
+	case cacheTypeMemory:
+		if o.memoryMaxEntries <= 0 {
+			o.memoryMaxEntries = 100
+		}
+		o.cache = memory.NewMemoryCache(o.memoryMaxEntries)
+	case cacheTypeLRU:
+		if o.lruMaxEntries <= 0 {
+			o.lruMaxEntries = 100
+		}
+		o.cache = lru.NewLRU(o.lruMaxEntries)
+	case cacheTypeCustom:
+		if o.cache == nil {
+			log.Fatal("empty cache")
+		}
+	}
+
+	return &bridger{
+		Cache: o.cache,
+	}
+}
+
+func (c *bridger) FetchWithJson(ctx context.Context, key string, fetcher Fetcher, model interface{}) (interface{}, error) {
+	return FetchWithJson(ctx, c.Cache, key, fetcher, model)
+}
+
+func (c *bridger) FetchWithString(ctx context.Context, key string, fetcher Fetcher) (string, error) {
+	return FetchWithString(ctx, c.Cache, key, fetcher)
+}
+
+func (c *bridger) FetchWithProtobuf(ctx context.Context, key string, fetcher Fetcher, model interface{}) (proto.Message, error) {
+	return FetchWithProtobuf(ctx, c.Cache, key, fetcher, model)
+}
+
+func (c *bridger) FetchWithNumber(ctx context.Context, key string, fetcher Fetcher) (float64, error) {
+	return FetchWithNumber(ctx, c.Cache, key, fetcher)
+}
+
+func (c *bridger) FetchWithArray(ctx context.Context, key string, fetcher Fetcher, model interface{}) (interface{}, error) {
+	return FetchWithArray(ctx, c.Cache, key, fetcher, model)
+}
+
+func (c *bridger) FetchWithIncludeKeys(ctx context.Context, output CacheValueOutput, empty EmptyCache, dec Decoder, otherKeys ...string) error {
+	return FetchWithIncludeKeys(ctx, c.Cache, output, empty, dec, otherKeys...)
+}
+
+func (c *bridger) FetchWithKeys(ctx context.Context, keys ...string) ([]interface{}, error) {
+	return FetchWithKeys(ctx, c.Cache, keys...)
+}
--- cache.go
+++ cache.go
@@ -3,20 +3,15 @@ package go_cache
 import (
 	"context"
 	"fmt"
-	"log"
 	"reflect"
 	"time"
 
-	"github.com/go-redis/redis/v8"
 	"github.com/golang/groupcache/singleflight"
 	"github.com/golang/protobuf/proto"
-	"github.com/liyanbing/go-cache/cacher/lru"
-	"github.com/liyanbing/go-cache/cacher/memory"
 	"github.com/liyanbing/go-cache/errors"
 	"github.com/liyanbing/go-cache/tools"
 
 	jsonIter "github.com/json-iterator/go"
-	redisCache "github.com/liyanbing/go-cache/cacher/redis"
 )
 
 /**
@@ -53,17 +48,6 @@ type Cache interface {
 	MGet(ctx context.Context, keys ...string) ([]interface{}, error)
 	// remove value by key
 	Remove(ctx context.Context, key string) error
-}
-
-type Bridge interface {
-	Cache
-	FetchWithJson(ctx context.Context, key string, fetcher Fetcher, model interface{}) (interface{}, error)
-	FetchWithString(ctx context.Context, key string, fetcher Fetcher) (string, error)
-	FetchWithProtobuf(ctx context.Context, key string, fetcher Fetcher, model interface{}) (proto.Message, error)
-	FetchWithNumber(ctx context.Context, key string, fetcher Fetcher) (float64, error)
-	FetchWithArray(ctx context.Context, key string, fetcher Fetcher, model interface{}) (interface{}, error)
-	FetchWithIncludeKeys(ctx context.Context, output CacheValueOutput, empty EmptyCache, dec Decoder, otherKeys ...string) error
-	FetchWithKeys(ctx context.Context, keys ...string) ([]interface{}, error)
 }
 
 func FetchWithJson(ctx context.Context, cache Cache, key string, fetcher Fetcher, model interface{}) (interface{}, error) {
@@ -161,141 +145,4 @@ func FetchWithIncludeKeys(ctx context.Context, cache Cache, output CacheValueOut
 
 func FetchWithKeys(ctx context.Context, cache Cache, keys ...string) ([]interface{}, error) {
 	return cache.MGet(ctx, keys...)
-}
-
-type cacheType int8
-
-const (
-	cacheTypeRedis cacheType = iota
-	cacheTypeMemory
-	cacheTypeLRU
-	cacheTypeCustom
-)
-
-type option struct {
-	cacheType        cacheType
-	redisCli         *redis.Client
-	cache            Cache
-	memoryMaxEntries int32
-	lruMaxEntries    int
-}
-
-func defaultOption() option {
-	return option{
-		cacheType:     cacheTypeLRU,
-		lruMaxEntries: 100,
-	}
-}
-
-type BridgeOption func(option)
-
-func WithRedis(cli *redis.Client) BridgeOption {
-	return func(o option) {
-		o.cacheType = cacheTypeRedis
-		o.redisCli = cli
-	}
-}
-
-func WithMemory(maxEntries int32) BridgeOption {
-	return func(o option) {
-		o.cacheType = cacheTypeMemory
-		o.memoryMaxEntries = maxEntries
-	}
-}
-
-func WithLRU(maxEntries int32) BridgeOption {
-	return func(o option) {
-		o.cacheType = cacheTypeLRU
-
-	}
-}
-
-func WithCache(cache Cache) BridgeOption {
-	return func(o option) {
-		o.cacheType = cacheTypeCustom
-		o.cache = cache
-	}
-}
-
-type bridger struct {
-	cacheType cacheType
-	Cache
-	redisCli redis.Client
-}
-
-func NewBridge(opts ...BridgeOption) Bridge {
-	o := defaultOption()
-	for _, opt := range opts {
-		opt(o)
-	}
-
-	switch o.cacheType {
-	case cacheTypeRedis:
-		if o.redisCli == nil {
-			log.Fatal("empty redis client")
-		}
-		o.cache = redisCache.NewRedisCache(o.redisCli)
-	case cacheTypeMemory:
-		if o.memoryMaxEntries <= 0 {
-			o.memoryMaxEntries = 100
-		}
-		o.cache = memory.NewMemoryCache(o.memoryMaxEntries)
-	case cacheTypeLRU:
-		if o.lruMaxEntries <= 0 {
-			o.lruMaxEntries = 100
-		}
-		o.cache = lru.NewLRU(o.lruMaxEntries)
-	case cacheTypeCustom:
-		if o.cache == nil {
-			log.Fatal("empty cache")
-		}
-	}
-
-	return &bridger{
-		Cache: o.cache,
-	}
-}
-
-func (c *bridger) SetNamespace(namespace string) {
-	c.SetNamespace(namespace)
-}
-
-func (c *bridger) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
-	return c.Set(ctx, key, value, expiration)
-}
-
-func (c *bridger) Get(ctx context.Context, key string) (interface{}, error) {
-	return c.Get(ctx, key)
-}
-
-func (c *bridger) Remove(ctx context.Context, key string) error {
-	return c.Remove(ctx, key)
-}
-
-func (c *bridger) FetchWithJson(ctx context.Context, key string, fetcher Fetcher, model interface{}) (interface{}, error) {
-	return FetchWithJson(ctx, c.Cache, key, fetcher, model)
-}
-
-func (c *bridger) FetchWithString(ctx context.Context, key string, fetcher Fetcher) (string, error) {
-	return FetchWithString(ctx, c.Cache, key, fetcher)
-}
-
-func (c *bridger) FetchWithProtobuf(ctx context.Context, key string, fetcher Fetcher, model interface{}) (proto.Message, error) {
-	return FetchWithProtobuf(ctx, c.Cache, key, fetcher, model)
-}
-
-func (c *bridger) FetchWithNumber(ctx context.Context, key string, fetcher Fetcher) (float64, error) {
-	return FetchWithNumber(ctx, c.Cache, key, fetcher)
-}
-
-func (c *bridger) FetchWithArray(ctx context.Context, key string, fetcher Fetcher, model interface{}) (interface{}, error) {
-	return FetchWithArray(ctx, c.Cache, key, fetcher, model)
-}
-
-func (c *bridger) FetchWithIncludeKeys(ctx context.Context, output CacheValueOutput, empty EmptyCache, dec Decoder, otherKeys ...string) error {
-	return FetchWithIncludeKeys(ctx, c.Cache, output, empty, dec, otherKeys...)
-}
-
-func (c *bridger) FetchWithKeys(ctx context.Context, keys ...string) ([]interface{}, error) {
-	return FetchWithKeys(ctx, c.Cache, keys...)
 }
